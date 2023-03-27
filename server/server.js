e8a81daffacd71933eb2be5c250dd1559a47aa9f
kiyoyoneko <kiyoedm@gmail.com>
2023-03-27 06:18:28 -0500

    purge unused

--- server/server.js
+++ server/server.js
@@ -4,16 +4,17 @@ const cors = require('cors')
 const PORT = process.env.PORT || 8080;
 const mongoose = require('mongoose');
 const app = require('./test');
+const axios = require('axios')
 
 require('dotenv/config')
 
-/*server.use(
+server.use(
   express.urlencoded({
     extended: true,
   })
 );
 server.use(cors())
-server.use(express.json());*/
+server.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
   server.use(express.static('client/build'));
@@ -27,7 +28,12 @@ connection.once('open',()=>{console.log('mongo connected!')})
 console.log(process.env.MONGODB_URI)
 const usersRouter = require('./routes/users')
 server.use('/users',usersRouter)
-
+axios.get(`http://localhost:${PORT}/users/`)
+          .then(response => {
+            console.log(this.List())
+          })
+          .catch((error) => {
+            console.log(error);
+          })
 // Start the API server
-
-app.listen(3001, () => console.log('Local app listening on port 3000!'));
+app.listen(PORT, () => console.log('Local app listening'));
