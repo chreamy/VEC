b98cc6966b96f93f72ad3f598ec358f1058011d3
kiyoyoneko <kiyoedm@gmail.com>
2023-03-27 23:25:42 -0500

    fuck mongodb

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,13 +3,13 @@ const router = require('express').Router();
 let User = require('../schemas/users');
 
 router.route('/').get((req, res) => {
+  console.log('requested')
   User.find()
     .then(users => res.json(users))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 router.route('/add').post((req, res) => {
   const username = req.body.username;
-
   const newUser = new User({username});
 
   newUser.save()
--- server/server.js
+++ server/server.js
@@ -3,9 +3,7 @@ const server = express();
 const cors = require('cors')
 const PORT = process.env.PORT || 8080;
 const mongoose = require('mongoose');
-const app = require('./test');
-const axios = require('axios')
-
+let User = require('./schemas/users.js');
 require('dotenv/config')
 
 server.use(
@@ -15,25 +13,11 @@ server.use(
 );
 server.use(cors())
 server.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === 'production') {
-  server.use(express.static('client/build'));
-}
-
-
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true,useUnifiedTopology: true });
 const connection = mongoose.connection;
 connection.once('open',()=>{console.log('mongo connected!')})
-console.log(process.env.MONGODB_URI)
 const usersRouter = require('./routes/users')
 server.use('/users',usersRouter)
-axios.get(`http://localhost:${PORT}/users/`)
-          .then(response => {
-            console.log(this.List())
-          })
-          .catch((error) => {
-            console.log(error);
-          })
 // Start the API server
-app.listen(PORT, () => console.log('Local app listening'));
+server.listen(PORT, () => console.log('Local app listening'));
+console.log(User.find().json)
